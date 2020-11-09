235fd6d4fc37bc0f521f01ac1464edb9ebfb7a2e
SeonHyungJo <seonhyung.jo@gmail.com>
2020-11-10 01:03:16 +0900

    update : Create PR_QUERY

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -2,4 +2,13 @@ const { request } = require('./request');
 const Query = require('./query');
 
 // Login User
-request({query: Query.LOGIN_QUERY}).then(data => console.log(data))
+request({query: Query.LOGIN_QUERY}).then(data => {
+  console.log('===LOGIN_QUERY===')
+  console.log(data)
+})
+
+// PR
+request({query: Query.PR_QUERY}).then(data => {
+  console.log('===PR_QUERY===')
+  console.dir(data)
+})
--- graphql-server/query.js
+++ graphql-server/query.js
@@ -1,8 +1,64 @@
-exports.LOGIN_QUERY = JSON.stringify({
+const createQuery = ({ query, variables = {} }) => {
+  return JSON.stringify({ query, variables })
+}
+
+exports.LOGIN_QUERY = createQuery({
   query: `query { 
     viewer { 
       login  
     }
-  }`,
-  variables: {}
+  }`
 });
+
+exports.PR_QUERY = createQuery({
+  query: `{
+    viewer {
+      repository(name: "Dev-Docs") {
+        pullRequests(orderBy: {field: CREATED_AT, direction: DESC}, first: 20, states: OPEN) {
+          nodes {
+            title
+            url
+            bodyHTML
+            createdAt
+            author {
+              avatarUrl
+              url
+              login
+              ... on User {
+                id
+                email
+                url
+                bioHTML
+              }
+            }
+            labels(first: 10) {
+              edges {
+                node {
+                  id
+                  color
+                  name
+                }
+              }
+            }
+            reviewRequests(first: 10) {
+              edges {
+                node {
+                  id
+                  requestedReviewer {
+                    ... on User {
+                      id
+                      email
+                      login
+                      name
+                      avatarUrl
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }`
+})
