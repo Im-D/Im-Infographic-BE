c317e461ac37ea0c12475f0580fb21bc6c89c932
SeonHyungJo <seonhyung.jo@gmail.com>
2020-11-23 22:59:18 +0900

    update : other data info

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -8,42 +8,59 @@ const UserQuery = require('./query/User.js');
 const IMDQuery = require('./query/IMD.js');
 const RepoQuery = require('./query/Repository.js');
 
-// // Author
-// request({ query: UserQuery.AUTHOR_QUERY }).then(({ data }) => {
-//   console.log('===Author===')
+// Author
+request({ query: UserQuery.AUTHOR_QUERY }).then(({ data }) => {
+  console.log('===Author===')
 
-//   const edges = data.viewer.repository.collaborators.edges || []
-//   const collaborators = edges
-//     .filter((item) => item.node.id !== 'MDQ6VXNlcjYwOTIwMTYw')
-//     .reduce((acc, { node }) => {
-//       acc[node.id] = node
-//       return acc
-//     }, {})
+  const edges = data.viewer.repository.collaborators.edges || []
+  const collaborators = edges
+    .filter((item) => item.node.id !== 'MDQ6VXNlcjYwOTIwMTYw')
+    .reduce((acc, { node }) => {
+      acc[node.id] = node
+      return acc
+    }, {})
 
-//   writeFile({ fileDirName: 'author', data: collaborators })
-// })
+  creatCommit({
+    path: 'author', 
+    fileName: getTodayFileName(), 
+    contents: JSON.stringify(collaborators)
+  })
+})
 
-// // TEAM Info
-// request({ query: IMDQuery.IMD_INFO_QUERY }).then(({ data: { viewer } }) => {
-//   console.log('===IMD Info ===')
-//   writeFile({ fileDirName: 'imd_info', data: viewer })
-// })
+// TEAM Info
+request({ query: IMDQuery.IMD_INFO_QUERY }).then(({ data: { viewer } }) => {
+  console.log('===IMD Info ===')
+  creatCommit({
+    path: 'imd_info', 
+    fileName: getTodayFileName(), 
+    contents: JSON.stringify(viewer)
+  })
+})
 
-// // TEAM Repos
-// request({ query: IMDQuery.IMD_REPOS_QUERY }).then(({ data: { viewer: { repositories } } }) => {
-//   console.log('===IMD Team ===')
-//   const data = {
-//     repos: repositories.nodes,
-//     totalCount: repositories.totalCount
-//   }
-//   writeFile({ fileDirName: 'imd_repos', data })
-// })
+// TEAM Repos
+request({ query: IMDQuery.IMD_REPOS_QUERY }).then(({ data: { viewer: { repositories } } }) => {
+  console.log('===IMD Team ===')
+  const data = {
+    repos: repositories.nodes,
+    totalCount: repositories.totalCount
+  }
 
-// // Repo Info
-// request({ query: RepoQuery.REPO_INFO_QUERY }).then(({ data: { viewer: { repository } } }) => {
-//   console.log('===Repo Info ===')
-//   writeFile({ fileDirName: 'repo_info', data: repository })
-// })
+  creatCommit({
+    path: 'imd_repos', 
+    fileName: getTodayFileName(), 
+    contents: JSON.stringify(data)
+  })
+})
+
+// Repo Info
+request({ query: RepoQuery.REPO_INFO_QUERY }).then(({ data: { viewer: { repository } } }) => {
+  console.log('===Repo Info ===')
+  creatCommit({
+    path: 'repo_info', 
+    fileName: getTodayFileName(), 
+    contents: JSON.stringify(repository)
+  })
+})
 
 // PR
 request({ query: RepoQuery.PR_QUERY }).then(({ data }) => {
