99275979749a8870e9bf828c1ac5d31b64659e78
SeonHyungJo <seonhyung.jo@gmail.com>
2020-11-28 01:13:32 +0900

    fix : 달 날짜가 이상하게 나오는 현상 해결

--- graphql-server/utils/Date.js
+++ graphql-server/utils/Date.js
@@ -3,5 +3,5 @@ const getNow = () => new Date()
 exports.getTodayFileName = () => {
   const now = getNow()
 
-  return `${now.getFullYear()}${now.getMonth()}${now.getDate()}`
+  return `${now.getFullYear()}${now.getMonth() + 1}${now.getDate()}`
 }
